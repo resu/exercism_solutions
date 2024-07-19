79195bc98a7b32f322d8e82b04f6d547d1e558d8
resu <resu@resu.sh>
2024-07-18 19:40:09 -0700

    Completed test of JS/vehicle-purchase

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,7 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
+  return kind === 'car' || kind === 'truck';
 }
 
 /**
@@ -23,7 +23,8 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
+  const betterOption = option1 < option2 ? option1 : option2;
+  return `${betterOption} is clearly the better choice.`;
 }
 
 /**
@@ -35,5 +36,11 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  if (age < 3) {
+    return originalPrice * 0.8;
+  } else if (age <= 10) {
+    return originalPrice * 0.7;
+  } else {
+    return originalPrice * 0.5;
+  }
 }
