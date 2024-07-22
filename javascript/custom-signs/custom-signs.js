e89242ac9a4b2db25b35de3b5bc44553f6d55bf2
resu <resu@resu.sh>
2024-07-22 09:27:32 -0700

    Completed test of JS/custom-signs

--- javascript/custom-signs/custom-signs.js
+++ javascript/custom-signs/custom-signs.js
@@ -14,7 +14,7 @@
  */
 
 export function buildSign(occasion, name) {
-  throw new Error('Implement the buildSign function');
+  return `Happy ${occasion} ${name}!`;
 }
 
 /**
@@ -26,7 +26,7 @@ export function buildSign(occasion, name) {
  */
 
 export function buildBirthdaySign(age) {
-  throw new Error('Implement the buildBirthdaySign function');
+  return `Happy Birthday! What a ${age < 50 ? 'young' : 'mature'} fellow you are.`;
 }
 
 /**
@@ -39,7 +39,7 @@ export function buildBirthdaySign(age) {
  */
 
 export function graduationFor(name, year) {
-  throw new Error('Implement the graduationFor function');
+  return `Congratulations ${name}!\nClass of ${year}`;
 }
 
 /**
@@ -53,5 +53,8 @@ export function graduationFor(name, year) {
  */
 
 export function costOf(sign, currency) {
-  throw new Error('Implement the costOf function');
+  const basePrice = 20;
+  const costPerCharacter = 2;
+  const cost = basePrice + (sign.length * costPerCharacter);
+  return `Your sign costs ${cost.toFixed(2)} ${currency}.`;
 }
