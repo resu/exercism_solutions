61d7aa39ed1874c4f6b9b5893b6aa0faf474067e
resu <resu@resu.sh>
2024-07-22 13:21:41 -0700

    Completed test of JS/windowing-system

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -5,3 +5,63 @@
  * exercise in this file. Do not forget to export the entities
  * you defined so they are available for the tests.
  */
+
+class Size {
+  constructor(width = 80, height = 60) {
+    this.width = width;
+    this.height = height;
+  }
+
+  resize(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Position {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  move(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class ProgramWindow {
+  constructor() {
+    this.size = new Size();
+    this.position = new Position();
+    this.screenSize = new Size(800, 600);
+  }
+
+  resize(newSize) {
+    const maxWidth = this.screenSize.width - this.position.x;
+    const maxHeight = this.screenSize.height - this.position.y;
+
+    this.size.width = Math.max(1, Math.min(newSize.width, maxWidth));
+    this.size.height = Math.max(1, Math.min(newSize.height, maxHeight));
+  }
+
+  move(newPosition) {
+    const maxX = this.screenSize.width - this.size.width;
+    const maxY = this.screenSize.height - this.size.height;
+
+    this.position.x = Math.max(0, Math.min(newPosition.x, maxX));
+    this.position.y = Math.max(0, Math.min(newPosition.y, maxY));
+  }
+}
+
+function changeWindow(window) {
+  const newSize = new Size(400, 300);
+  const newPosition = new Position(100, 150);
+
+  window.resize(newSize);
+  window.move(newPosition);
+
+  return window;
+}
+
+export { Size, Position, ProgramWindow, changeWindow };
