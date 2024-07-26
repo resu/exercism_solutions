39f1591fbfc5665ad0146cee6af85518cbfde4f8
resu <resu@resu.sh>
2024-07-26 16:18:43 -0700

    Completed test of TS/resistor-color-duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,17 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedValue(colors: string[]): number {
+  const colorMap: { [key: string]: number } = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+
+  const value = colorMap[colors[0]] * 10 + colorMap[colors[1]];
+  return value;
 }
