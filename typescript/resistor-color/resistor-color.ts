f1ee43a8a31fe3aff66495c293c1b6416618d564
resu <resu@resu.sh>
2024-07-25 22:48:21 -0700

    Completed test of JS/resistor-color

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,16 @@
-export const colorCode = () => {
-  throw new Error('Delete this line and implement this function')
-}
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+] as const;
 
-export const COLORS = undefined
+type Color = typeof COLORS[number];
+
+export const colorCode = (color: Color) => COLORS.indexOf(color);
