90bbfc68de4913d581b478952365f429cbd76ba0
resu <resu@resu.sh>
2024-08-10 15:52:15 -0700

    Completed test of JS/resistor-color-duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decodedValue = (colors) => {
+  const colorMap = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+
+  return colorMap[colors[0]] * 10 + colorMap[colors[1]];
 };
